2383d50870bff4c7b6a096e1bab64b2fc812899a
Ben Cresswell <42128027+bjcresswell@users.noreply.github.com>
2024-06-24 19:38:35 +1000

    Add packages script

---
+++ code/packages.R
@@ -0,0 +1,43 @@
+## Packages required 
+
+### Wrangling
+library(hms)
+library(data.io)
+library(sourceRmd)
+library(shoRtcut)
+library(readxl)
+library(plotrix)    # for SEM calc 
+library(lubridate)  # dates
+library(vegan)
+library(iNEXT)
+library(magrittr)
+library(tidyverse)
+
+# Plotting
+library(patchwork)
+library(gghalves)
+library(cowplot)
+library(sjPlot)
+library(ggpubr)
+
+### Modeling
+library(glmmTMB)
+library(effects)   #for partial effects plots
+library(modelr)
+library(mgcv)
+library(brms)       #for fitting models in STAN
+library(boral)
+library(mvabund)
+library(rstan)
+library(coda)       #for diagnostics
+library(bayesplot)  #for diagnostics
+library(emmeans)    #for marginal means etc
+library(broom)      #for tidying outputs
+library(broom.mixed)
+library(DHARMa)     #for residual diagnostics
+library(MuMIn)
+library(tidybayes)  #for more tidying outputs
+library(ggeffects)  #for partial plots
+
+
+
