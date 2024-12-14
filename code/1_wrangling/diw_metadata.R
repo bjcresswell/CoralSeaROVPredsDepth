d229fa18f4e38e08512e3e550e45bf454a838d5e
Ben Cresswell <bjcresswell@gmail.com>
2024-12-14 11:32:26 -0500

    Tidy up wrangling folder

--- code/1_wrangling/diw_metadata.R
+++ code/1_wrangling/diw_metadata.R
@@ -1,23 +1,21 @@
 # Metadata wrangling
 
-source('code/packages.R')
+source('code/0_extras/packages.R')
 
 # Biogeographic parameters - these have already been determined
-reef_biogeo <- read_excel("data/reef_biogeo.xlsx", 2) %>% 
+reef_biogeo <- read_excel("data/1_mappingdata/reef_biogeo.xlsx", 2) |> 
   mutate_if(is.character, as.factor)          # Fix ch vars
 
 
 # Main metadata file (want a copy of the full metadata so will import that first)
-rmetadata_raw <- data.io::read$csv("https://figshare.com/ndownloader/files/42424437?private_link=0a7edbfa91467638b998") 
-#save(rmetadata_raw, file = "data/Rdata/rmetadata_raw.Rdata")    
-#load("data/Rdata/rmetadata_raw.Rdata")    
+#rmetadata_raw <- data.io::read$csv("https://figshare.com/ndownloader/files/42424437?private_link=0a7edbfa91467638b998") 
+#save(rmetadata_raw, file = "data/2_Rdata/rmetadata_raw.Rdata")    
+load("data/2_Rdata/rmetadata_raw.Rdata")    
 
 # Trim down to only transects on the outside of reefs and add other variables as required - leaves us with 496 transects
-rmetadata <- rmetadata_raw %>% 
-  #filter(Habitat != "lagoon") %>%  # Get rid of lagoon transects early
-  filter(Situation != "inner") %>%  # Get rid of any transect not on an outside reef
-  droplevels() %>% 
-  mutate(Aspect_descriptive = replace(Aspect_descriptive, Aspect_descriptive == "modertae_slope", "moderate_slope")) %>%  # Fix variable spelling
+rmetadata <- rmetadata_raw |> 
+  filter(Situation != "inner")  |>  # Get rid of any transect not on an outside reef
+  droplevels() |> 
   mutate(T_ID = paste(Survey, Site, Survey_Day, Dive_No, Transect, sep = "-"),  # T_ID variable for wrangling
          Depth_bin = factor(case_when(Depth_m < 11 ~ '0-10',
                                       Depth_m >= 11  & Depth_m < 21 ~ '11-20', 
@@ -31,22 +29,14 @@ rmetadata <- rmetadata_raw %>%
                                       Depth_m >= 91  ~ '91-100'), ordered = TRUE),
          Depth_bin_meso = fct_relevel(factor(case_when(Depth_m < 31 ~ 'Altiphotic',
                                               Depth_m >= 31  & Depth_m < 61 ~ 'Upper_mesophotic', 
-                                              Depth_m >= 61   ~ 'Lower_mesophotic')), "Altiphotic", "Upper_mesophotic")) %>% 
+                                              Depth_m >= 61   ~ 'Lower_mesophotic')), "Altiphotic", "Upper_mesophotic"))  |> 
   mutate(Depth = factor(case_when(grepl("Altiphotic", Depth_bin_meso) ~ "Shallow",
                                   grepl("Upper_mesophotic", Depth_bin_meso) ~ "Upper",
-                                  grepl("Lower_mesophotic", Depth_bin_meso) ~ "Lower"))) %>% 
+                                  grepl("Lower_mesophotic", Depth_bin_meso) ~ "Lower"))) |> 
   mutate(Depth = fct_relevel(Depth, c("Shallow", "Upper")))  |>
   mutate(Site_Depth = factor(paste(Site, Depth, sep = "_")))  |>  
   left_join(reef_biogeo) %>%  # Combine with biogeographic metadata
   mutate_if(is.character, as.factor) %>%             # Fix ch vars
-  mutate(Region = fct_relevel(Region, "Far_North", "North")) %>% 
-  mutate(Reef_size = factor(case_when(Reef_1_Area < 181 ~ 'Small',
-                                      Reef_1_Area >= 181 & Reef_1_Area < 919 ~ 'Medium',
-                                      Reef_1_Area > 919 ~ 'Large'))) %>% 
-  mutate(#Isol = sqrt(dGBR) / sqrt(Reef_100),      
-         Isol = sqrt(dGBR+dNN) / sqrt(Reef_1_Area + Reef_100),
-         IsolRank = dense_rank(desc(Isol))) %>% 
-  mutate(Reef_1 = fct_reorder(Reef_1, desc(Isol))) %>% 
   mutate(Zone = factor(                # Ideally want to get this all into the metadata
     case_when(
       Reef_1 == "Bougainville" & Site_long < 147.108883 & Site_lat > -15.495 | 
@@ -62,15 +52,17 @@ rmetadata <- rmetadata_raw %>%
         Reef_1 == "Saumarez" |
         Reef_1 == "Willis" |
         Reef_1 == "Wreck" 
-      ~ "HPZ", .default = "NTMR"))) %>% 
+      ~ "HPZ", .default = "NTMR"))) |> 
     tibble()
 
 
-lob_metadata <- rmetadata %>%  filter(Reef_1 == "Osprey" | Reef_1 == "Bougainville" | Reef_1 == "Lihou") |>  # Need this for a full jo
+# We only need Lihou, Osprey and Bougainville for this analysis
+lob_metadata <- rmetadata  |> 
+  filter(Reef_1 == "Osprey" | Reef_1 == "Bougainville" | Reef_1 == "Lihou") |>  
   droplevels()
 
 
-#save(lob_metadata, file = "data/Rdata/lob_metadata.Rdata")    
-#load("data/Rdata/lob_metadata.Rdata")    
+#save(lob_metadata, file = "data/2_Rdata/lob_metadata.Rdata")    
+#load("data/2_Rdata/lob_metadata.Rdata")    
 
 
--- code/1_wrangling/diw_rawdata.R
+++ code/1_wrangling/diw_rawdata.R
@@ -1,67 +1,63 @@
-## Wrangle in Osprey and Bougainville data
+## Wrangle in fish data and extract predator observations
 
 
 # Housekeeping + prelims
-#rm(list=ls())
+rm(list=ls())
 #dev.off()
-source("code/packages.R")
+source("code/0_extras/packages.R")
 
 # Load main fish data - 15274 rows 18487
-fish_raw <- data.io::read$csv("https://figshare.com/ndownloader/files/42424440?private_link=0a7edbfa91467638b998") %>% 
-  mutate_if(is.character, as.factor) %>%                      # Fix ch vars
-  #mutate(Aspect_descriptive = replace(Aspect_descriptive, Aspect_descriptive == "modertae_slope", "moderate_slope")) %>%  # Fix variable spelling - now fixed at source
-  tibble()
+# fish_raw <- data.io::read$csv("https://figshare.com/ndownloader/files/42424440?private_link=0a7edbfa91467638b998") |> 
+#   mutate_if(is.character, as.factor) |>                    # Fix ch vars
+#   tibble()
 
-#save(fish_raw, file = "data/Rdata/fish_raw.Rdata")    
-load("data/Rdata/fish_raw.Rdata")    
+#save(fish_raw, file = "data/2_Rdata/fish_raw.Rdata")    
+load("data/2_Rdata/fish_raw.Rdata")    
 
-
-
-#glimpse(fish)
-#head(fish)
+#glimpse(fish_raw)
+#head(fish_raw)
 
 # Load metadata
-source("code/wrangling/diw_metadata.R")
+source("code/1_wrangling/diw_metadata.R")
 # OR
 #load("data/Rdata/lob_metadata.Rdata")    
 
-# Trip down to Bougainville and Osprey and add metadata
+# Combine fish and metadata
 lob_fish <- 
-  fish_raw %>% 
-  filter(Reef_1 == "Osprey" | Reef_1 == "Bougainville" | Reef_1 == "Lihou") %>%
-  full_join(lob_metadata) %>% 
-  filter(Situation != "inner") %>%  # Get rid of any transect not on an outside reef
+  fish_raw  |> 
+  right_join(lob_metadata) |> 
   mutate(Number = replace_na(Number, 0)) |>   # Need to replace NAs in transects where there were no observations
   droplevels()
 
 
-lob_fish %>% 
-  select(Reef_1, Site, T_ID) %>%
+# Save/load
+#save(lob_fish, file = "data/2_Rdata/lob_fish.Rdata")    
+
+# Checks
+lob_fish  |> 
+  select(Reef_1, Site, T_ID) |> 
   unique()
 # 346 transects
 
 # Any transects with no fish?
-lob_fish %>% filter(is.na(OpCode))
-# 6 = Osprey, Feb 21, Dive 2, T12
+lob_fish |> filter(is.na(OpCode))
+# 6 
 
 # Depths
 lob_fish  %$% 
   summary(Depth)
 
-# Checks - these should all be 0 - if not, then likely it is the inclusion of lagoon or inner transects that have snuck in somehow
+# Detailed checks - these should all be 0
+# If not, then likely it is the inclusion of lagoon or inner transects
 check1 <- anti_join(lob_fish, lob_metadata)
 check2 <- anti_join(lob_metadata, lob_fish)
-check3 <- check1 %>% 
-  select(Reef_1, Site, T_ID) %>%
+check3 <- check1  |> 
+  select(Reef_1, Site, T_ID) |> 
   unique()
 
 
-# Save if required
-#save(lob_fish, file = "data/Rdata/lob_fish.Rdata")    
-
-
 # Extract predator fishes, based on established criteria - 795 total actual obs
-lob_preds <- lob_fish %>% 
+lob_preds <- lob_fish |> 
   filter(str_detect(Functional.Group, "pisc") |
            str_detect(Functional.Group, "carn") |
            str_detect(Trophic.Group, "pisc") |
@@ -82,9 +78,9 @@ lob_preds <- lob_fish %>%
            Genus != "Pyronotanthias" &  # .. as above
            Genus != "Liopropoma" &  # .. as above
            Functional.Group != "cleaner" & # And remoras
-           Trophic.Level >= 3.3) %>% 
+           Trophic.Level >= 3.3)  |> 
   full_join(lob_metadata) %>%                 # The full join with the rmetadata needs to happen before...
-  mutate(Number = replace_na(Number, 0)) %>% 
+  mutate(Number = replace_na(Number, 0)) |> 
   mutate(Family = replace(Family, Family == "Epinephelidae", "Serranidae"))  |> 
   droplevels()
 
@@ -93,45 +89,45 @@ lob_preds <- lob_fish %>%
 
 # Should now just have Lihou, Osprey, Bougainville
 lob_preds %$% summary(Reef_1)
+## Yes, and reasonably balanced
 
 # Check that all have a Zone applied
 lob_preds %$% summary(Zone)
 
 # Number of individual pred observations - use both preds and predsum to check consistency
-#lob_predsum %>% summarise(Total_preds = sum(No_Preds))
-lob_preds %>% summarise(No_Preds =sum(Number)) 
+#lob_predsum  |>  summarise(Total_preds = sum(No_Preds)) # Can only run this once predsum loaded
+lob_preds  |>  summarise(No_Preds =sum(Number)) 
 
 ## No of families
-lob_preds %>% group_by(Family) %>% summarise() # 18 fams total
+lob_preds |>  group_by(Family) |>  summarise() # 18-1 = 17 fams total
 
 # Number of predator species (78- NA = 77)
-(lob_predtaxa <- lob_preds %>% group_by(Binomial) %>% summarise()) # 77
+(lob_predtaxa <- lob_preds |>  group_by(Binomial) |>  summarise()) # 77
 
 # Check number of transects: 346
-lob_preds %>% select(T_ID) %>%  distinct()
-lob_preds %>% group_by(T_ID) %>%  summarise()
+lob_preds |>  select(T_ID)  |>   distinct()
+#lob_preds  |>  group_by(T_ID)  |>  summarise()
 
 # Check number of transects per depth zone - can use lob_preds or lob_predsum
-lob_predsum %>% group_by(Depth)  %$% summary(Depth) 
-lob_preds %>% distinct(Depth, T_ID)  %$% summary (Depth)
+#lob_predsum %>% group_by(Depth)  %$% summary(Depth) 
+lob_preds |>  distinct(Depth, T_ID)  %$% summary (Depth)
 
 # Check how many are zeros - 98
-lob_preds %>% filter(is.na(OpCode))
+lob_preds |>  filter(is.na(OpCode))
 
 # Should have no NAs in the Number var
 lob_preds %$% summary(Number)
 
 
 # Some other checks
-## By body size
+## By body size - note we do have some NAs as not all videos were stereo
 lob_preds %$% summary(Length_mm) # Some crazy small measurements in here - pass on to GFG
-small_preds <- lob_preds %>% filter(Length_mm <200) # 
+small_preds <- lob_preds |>  filter(Length_mm <200) # 
 #lg_preds <- lob_preds %>% filter(Length_mm >300) # 
 
 
-
-# Save once happy, and if required
-#save(lob_preds, file = "data/Rdata/lob_preds.Rdata")    
-#load("data/Rdata/lob_preds.Rdata")    
+# Save/load
+#save(lob_preds, file = "data/2_Rdata/lob_preds.Rdata")    
+#load("data/2_Rdata/lob_preds.Rdata")    
 
 
